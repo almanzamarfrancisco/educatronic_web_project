55dcfbf8a3c18345db675cf2185097c8f8b63ac9
Ale Almanza <almanza.marfrancisco@hotmail.com>
2024-08-24 23:17:37 -0600

    Progress: Execises transition completed

--- backend_mongoose/main.c
+++ backend_mongoose/main.c
@@ -36,22 +36,22 @@ typedef struct PROGRAM_FILE {
     int fileId;
     char *name;
     char *code;
-    int robot_id;
+    int exercise_id;
 } program_file;
-typedef struct ROBOT {
-    int robot_id;
+typedef struct EXERCISE {
+    int exercise_id;
     char *name;
-    u_int16_t angle;
+    char *content;
     u_int16_t program_files_count;
-} robot;
-robot *robots = NULL;
+} exercise;
+exercise *exercises = NULL;
 program_file *programs = NULL;
-size_t robots_count = 0;
+size_t exercises_count = 0;
 size_t programs_count = 0;
 
 const char * program_file_properties[] = {
     "programId",
-    "robotId",
+    "exerciseId",
     "code",
 };
 // Function to initialize GPIO
@@ -157,19 +157,19 @@ int update_file(struct mg_str json, int execute) {
     if (execute) execute_commands(code);
     return 0;
 }
-static int robots_callback(void *NotUsed, int argc, char **argv, char **azColName){
+static int exercises_callback(void *NotUsed, int argc, char **argv, char **azColName){
     (void)NotUsed;(void)argc;(void)azColName;
-    robots = realloc(robots, (robots_count + 1) * sizeof(robot));
-    if (!robots) {
-        fprintf(stderr, "Memory allocation failed\n");
+    exercises = realloc(exercises, (exercises_count + 1) * sizeof(exercise));
+    if (!exercises) {
+        fprintf(stderr, "Memory allocation failed in exercises\n");
         exit(1);
     }
-    for(size_t i = 0; i < robots_count; i++){ if(robots[i].robot_id == atoi(argv[0])) return 0;}
-    robots[robots_count].robot_id = atoi(argv[0]);
-    robots[robots_count].name = strdup(argv[1]);
-    robots[robots_count].angle = atoi(argv[2]);
-    robots[robots_count].program_files_count = 0;
-    robots_count++;
+    for(size_t i = 0; i < exercises_count; i++){ if(exercises[i].exercise_id == atoi(argv[0])) return 0;}
+    exercises[exercises_count].exercise_id = atoi(argv[0]);
+    exercises[exercises_count].name = strdup(argv[1]);
+    exercises[exercises_count].content = strdup(argv[2]);
+    exercises[exercises_count].program_files_count = 0;
+    exercises_count++;
     return 0;
 }
 static int programs_callback(void *NotUsed, int argc, char **argv, char **azColName){
@@ -183,21 +183,22 @@ static int programs_callback(void *NotUsed, int argc, char **argv, char **azColN
     programs[programs_count].fileId = atoi(argv[0]);
     programs[programs_count].name = strdup(argv[1]);
     programs[programs_count].code = strdup(argv[2]);
-    programs[programs_count].robot_id = atoi(argv[3]);
+    programs[programs_count].exercise_id = atoi(argv[3]);
     programs_count++;
     return 0;
 }
-void load_robots_from_db(sqlite3 *db) {
+void load_exercises_from_db(sqlite3 *db) {
     char *err_msg = 0;
-    const char *robots_table = "SELECT * FROM Robots;";
-    int rc = sqlite3_exec(db, robots_table, robots_callback, 0, &err_msg);
+    const char *exercise_table = "SELECT * FROM Exercises;";
+    int rc = sqlite3_exec(db, exercise_table, exercises_callback, 0, &err_msg);
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "Failed to load robots: %s\n", err_msg);
+        fprintf(stderr, "Failed to load exercises: %s\n", err_msg);
         sqlite3_free(err_msg);
     } else {
-        printf("\t => Loaded %zu robots\n", robots_count);
-        for(size_t i = 0; i < robots_count; i++)
-            printf("\t\t=> Robot: %d - %s\n", robots[i].robot_id, robots[i].name);
+        printf("\t => Loaded %zu exercises\n", exercises_count);
+        for(size_t i = 0; i < exercises_count; i++)
+            printf("\t\t=> Exercise: %d - %s\n", exercises[i].exercise_id, exercises[i].name);
+        printf("----------------------------------------------------\n");
     }
 }
 void load_programs_from_db(sqlite3 *db) {
@@ -225,14 +226,14 @@ int sqlite3_init_database(){
         return 1;
     }
     const char *sql_init[] = {
-            "CREATE TABLE IF NOT EXISTS Robots(robot_id INTEGER PRIMARY KEY AUTOINCREMENT, robot_name TEXT, angle INTEGER);",
-            "CREATE TABLE IF NOT EXISTS ProgramFiles(programfile_id INTEGER PRIMARY KEY AUTOINCREMENT, program_file_name TEXT, content TEXT, robot_id INTEGER);",
-            // "INSERT INTO Robots(robot_name, angle) VALUES ('BASIC', 45);",
-            // "INSERT INTO Robots(robot_name, angle) VALUES ('ELEVATOR', 135);",
-            // "INSERT INTO ProgramFiles(program_file_name, content, robot_id) VALUES ('basic_first_file', '', 1);",
-            // "INSERT INTO ProgramFiles(program_file_name, content, robot_id) VALUES ('basic_second', '', 1);",
-            // "INSERT INTO ProgramFiles(program_file_name, content, robot_id) VALUES ('elevator_first_file', '', 2);",
-            // "INSERT INTO ProgramFiles(program_file_name, content, robot_id) VALUES ('elevator_second_file', '', 2);",
+            "DROP TABLE IF EXISTS Exercises",
+            "DROP TABLE IF EXISTS ProgramFiles",
+            "CREATE TABLE IF NOT EXISTS Exercises(exercise_id INTEGER PRIMARY KEY AUTOINCREMENT, exercise_name TEXT, content TEXT);",
+            "CREATE TABLE IF NOT EXISTS ProgramFiles(programfile_id INTEGER PRIMARY KEY AUTOINCREMENT, program_file_name TEXT, content TEXT, exercise_id INTEGER);",
+            "INSERT INTO Exercises(exercise_name, content) VALUES ('First Exercise', '1. Make the Doors open  for 1 second\n 2. Up to floor 1');",
+            "INSERT INTO Exercises(exercise_name, content) VALUES ('Second Exercise', '1. Make the Doors open  for 3 seconds\n 2. Up to floor 6');",
+            "INSERT INTO ProgramFiles(program_file_name, content, exercise_id) VALUES ('First try', 'O 2\nU 1', 1);",
+            "INSERT INTO ProgramFiles(program_file_name, content, exercise_id) VALUES ('Second try', 'O \nU 6', 2);",
         };
     for(long unsigned int i = 0; i < sizeof(sql_init)/sizeof(sql_init[0]); i++){
         rc = sqlite3_exec(db, sql_init[i], 0, 0, &err_msg);
@@ -244,7 +245,7 @@ int sqlite3_init_database(){
         }
     }
     load_programs_from_db(db);
-    load_robots_from_db(db);
+    load_exercises_from_db(db);
     sqlite3_close(db);
     return 0;
 }
@@ -260,7 +261,7 @@ void trim_non_alphanumeric(char *str) {
     }
     str[j] = '\0';
 }
-char * programs_to_json(){ // TODO: Make this and robots one function
+char * programs_to_json(){ // TODO: Make this and exercise one function
     char * result = (char*)malloc(sizeof(char)*2048);
     memset(result, 0, sizeof(char)*2048);
     for(size_t i = 0; i < programs_count; i++){
@@ -273,37 +274,39 @@ char * programs_to_json(){ // TODO: Make this and robots one function
             MG_ESC("fileId"), programs[i].fileId,
             MG_ESC("name"), name,
             MG_ESC("code"), code,
-            MG_ESC("robot_id"), programs[i].robot_id
+            MG_ESC("exercise_id"), programs[i].exercise_id
             );
         strcat(result, program_json);
         if(i < programs_count - 1){
             strcat(result, ",");
         }
-        for(size_t j = 0; j < robots_count; j++){
-            if(robots[j].robot_id == programs[i].robot_id){
-                robots[j].program_files_count++;
+        for(size_t j = 0; j < exercises_count; j++){
+            if(exercises[j].exercise_id == programs[i].exercise_id){
+                exercises[j].program_files_count++;
             }
         }
     }
     trim_non_alphanumeric(result);
     return result;
 }
-char * robots_to_json(){ // TODO: Make this and programs one function
+char * exercises_to_json(){ // TODO: Make this and programs one function
     char * result = (char*)malloc(sizeof(char)*2048);
     memset(result, 0, sizeof(char)*2048);
-    for(size_t i = 0; i < robots_count; i++){
-        char *robot_json = (char*)malloc(sizeof(char)*1024);
-        memset(robot_json, 0, sizeof(char)*1024);
+    for(size_t i = 0; i < exercises_count; i++){
+        char *exercises_json = (char*)malloc(sizeof(char)*1024);
+        memset(exercises_json, 0, sizeof(char)*1024);
         char name[50];
-        sprintf(name, "\"%s\"", robots[i].name);
-        robot_json = mg_mprintf("{%m:%d, %m:%s, %m:%hu, %m:%hu}",
-            MG_ESC("robot_id"), robots[i].robot_id,
+        char content[150];
+        sprintf(name, "\"%s\"", exercises[i].name);
+        sprintf(content, "\"%s\"", exercises[i].content);
+        exercises_json = mg_mprintf("{%m:%d, %m:%s, %m:%s, %m:%hu}",
+            MG_ESC("exercise_id"), exercises[i].exercise_id,
             MG_ESC("name"), name,
-            MG_ESC("angle"), robots[i].angle,
-            MG_ESC("program_files_count"), robots[i].program_files_count
+            MG_ESC("content"), content,
+            MG_ESC("program_files_count"), exercises[i].program_files_count
             );
-        strcat(result, robot_json);
-        if(i < robots_count - 1){
+        strcat(result, exercises_json);
+        if(i < exercises_count - 1){
             strcat(result, ",");
         }
     }
@@ -319,7 +322,7 @@ int update_database(){
         return 1;
     }
     load_programs_from_db(db);
-    load_robots_from_db(db);
+    load_exercises_from_db(db);
     sqlite3_close(db);
     return 0;
 }
@@ -332,8 +335,8 @@ int update_database(){
         if (mg_http_match_uri(hm, "/api/code/get_default")) {
             if (update_database() != 0){ printf("Database error: "); return; }
             char *programs_json = programs_to_json();
-            char *robots_json = robots_to_json();
-            json_response = mg_mprintf("{%m:[%s], %m:[%s]}", MG_ESC("exercises"), robots_json, MG_ESC("programs"), programs_json);
+            char *exercises_json = exercises_to_json();
+            json_response = mg_mprintf("{%m:[%s], %m:[%s]}", MG_ESC("exercises"), exercises_json, MG_ESC("programs"), programs_json);
             int content_length = strlen(json_response);
             mg_printf(c, CORS_HEADERS,
                       content_length);
@@ -434,7 +437,7 @@ int update_database(){
 int main(void) {
     struct mg_mgr mgr;                            // Event manager
     struct mg_connection *connection;
-    // pthread_t enconde_video_thread_id;
+    pthread_t enconde_video_thread_id;
     mg_log_set(MG_LL_INFO);                       // Set to 3 to enable debug
     mg_mgr_init(&mgr);                            // Initialise event manager
     connection = mg_http_listen(&mgr, s_http_addr, event_handler, NULL);  // Create HTTP listener
@@ -447,14 +450,14 @@ int main(void) {
     if (sqlite3_init_database() != 0){            // Initialize the database
         return 0;
     }
-    /*if(pthread_create(&enconde_video_thread_id, NULL, encode_video, NULL)) { // Starts live video
+    if(pthread_create(&enconde_video_thread_id, NULL, encode_video, NULL)) { // Starts live video
         fprintf(stderr, "Error creating econde viedo thread\n");
         return 1;
     }
     if(pthread_join(enconde_video_thread_id, NULL)) {
         fprintf(stderr, "Error joining encode video thread\n");
         return 2;
-    }*/
+    }
     for (;EVER;) mg_mgr_poll(&mgr, 500);           // Infinite event loop
     mg_mgr_free(&mgr);                            // Clears the connection manager
     return 0;
