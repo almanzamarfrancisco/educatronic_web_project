5d40d6d4371680c111e9923c6c0fb9d6cb768db1
Ale Almanza <almanza.marfrancisco@hotmail.com>
2025-03-10 17:08:57 -0600

    Checkpoint: mock for the server response. Continue with the front-end

--- backend_mongoose/server.c
+++ backend_mongoose/server.c
@@ -1,32 +1,47 @@
 #include "server.h"
-#include "database_management.h"
-#include "program_logic.h"
-#include "utils.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
-static const char *s_http_addr = "http://192.168.1.71:8000"; // Developing HTTP port
+#include "database_management.h"
+#include "program_logic.h"
+#include "utils.h"
+#define CORS_HEADERS                                                                   \
+    "HTTP/1.1 200 OK\r\n"                                                              \
+    "Content-Type: application/json\r\n"                                               \
+    "Access-Control-Allow-Origin: *\r\n"                                               \
+    "Access-Control-Allow-Methods: GET, POST, OPTIONS\r\n"                             \
+    "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept\r\n" \
+    "Content-Length: %d\r\n"                                                           \
+    "\r\n"
+
+static const char *s_http_addr = "http://192.168.1.71:8000";  // Developing HTTP port
+// static const char *s_http_addr = "http://localhost:8000";  // Ngrok HTTP port
 static const char *s_root_dir = "web_root";
 
 void event_handler(struct mg_connection *c, int ev, void *ev_data) {
     if (ev == MG_EV_HTTP_MSG) {
         struct mg_http_message *hm = (struct mg_http_message *)ev_data;
         if (mg_http_match_uri(hm, "/api/state")) {
-            printf("[I] Sending the state for this user...\n");
-
-            // JSON response for /api/state
-            const char *json_response =
-                "{"
-                "  \"programs\": ["
+            printf("\t [I] Sending the current state for this user...\n");
+            char *json_response = (char *)malloc(sizeof(char) * 1024);
+            const char *response =
+                "["
                 "    {\"id\": \"One\", \"name\": \"Primer archivo\", \"content\": \"I\\nS 6\\nP 2\\nB 3\\nF\"},"
                 "    {\"id\": \"Two\", \"name\": \"Segundo archivo\", \"content\": \"This is the content of the second file\"},"
                 "    {\"id\": \"Three\", \"name\": \"Versión definitiva\", \"content\": \"3rd file content\"}"
-                "  ]"
-                "}";
-
-            mg_http_reply(c, 200, "Content-Type: application/json\r\nAccess-Control-Allow-Origin: *\r\n", "%s", json_response);
-        }
-        else {
+                "]";
+            const char *exercises =
+                "["
+                "    {\"id\": \"One\", \"name\": \"Primer ejercicio\", \"content\": \"Contenido del primer ejercicio\"},"
+                "    {\"id\": \"Two\", \"name\": \"Segundo ejercicio\", \"content\": \"Este es el segundo ejercicio y su contenido\"},"
+                "    {\"id\": \"Three\", \"name\": \"Tercer ejercicio\", \"content\": \"Contenido del tercer ejercicio \\n lorem ipsum \"}"
+                "]";
+            json_response = mg_mprintf("{%m:%s, %m:%s}", MG_ESC("programs"), response, MG_ESC("exercises"), exercises);
+            int content_length = strlen(json_response);
+            mg_printf(c, CORS_HEADERS, content_length);
+            mg_printf(c, "%s\n", json_response);
+        } else {
             struct mg_http_serve_opts opts = {.root_dir = s_root_dir};
             mg_http_serve_dir(c, ev_data, &opts);
         }
@@ -39,14 +54,13 @@ void start_server() {
 
     mg_mgr_init(&mgr);
     connection = mg_http_listen(&mgr, s_http_addr, event_handler, NULL);
-    if (connection == NULL)
-    {
+    if (connection == NULL) {
         printf("Error initializing the server\n");
         return;
     }
     printf("HTTP server initialized on %s\n", s_http_addr);
     for (;;)
         mg_mgr_poll(&mgr, 500);
-    
+
     mg_mgr_free(&mgr);
 }
